1ada8b99f427d5ec76294b719c3044df378dc9d1
konushy <100558524+konushy@users.noreply.github.com>
2024-04-09 09:13:43 +0300

    Update qme.js

--- scripts/qme.js
+++ scripts/qme.js
@@ -1,1 +1,32 @@
-console.log('RUNNING SCRIPT (changed): qme');
+const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+
+const secret_name = "qme_temp";
+
+const client = new SecretsManagerClient({
+  region: "us-east-1",
+});
+
+async function main() {
+  let response;
+
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+
+  const secret = response.SecretString;
+
+  console.log(secret);
+
+  console.log('RUNNING SCRIPT: qme');
+}
+
+main();
