c5c7018f0909ca8216bff655e6ba1a128bf2e4f9
konushy <100558524+konushy@users.noreply.github.com>
2024-04-15 13:24:29 +0300

    Update ai.js

--- scripts/ai.js
+++ scripts/ai.js
@@ -12,16 +12,18 @@ async function getWebsiteContent() {
 }
 
 async function main() {
-    const websiteContent = await getWebsiteContent();
+    //const websiteContent = await getWebsiteContent();
     job_id = process.env.QME_JOB_ID;
     console.log('External VAR:' + job_id);
     console.log('RUNNING SCRIPT: ai');
+    /*
     if (websiteContent) {
         console.log('Content 1:0:');
         console.log('External VAR:' + job_id);
         console.log('RUNNING SCRIPT: ai');
         console.log(websiteContent);
     }
+    */
 }
 
 main().catch(error => {
